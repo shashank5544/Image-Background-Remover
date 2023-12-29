db480f4c6245f621cabc77040c979f6ed0934cf9
shashank5544 <85498017+shashank5544@users.noreply.github.com>
2023-12-29 10:39:41 +0530

    Update bg.py

--- bg.py
+++ bg.py
@@ -3,10 +3,10 @@ import requests
 response = requests.post(
     'https://api.remove.bg/v1.0/removebg',
     data={
-        'image_url': "https://user-images.githubusercontent.com/76609302/172825370-c323b652-ad37-4244-94ff-51105516039f.png",
+        'image_url': "Url of image you want to remove background of",
         'size': 'auto'
     },
-    headers={'X-Api-Key': 'NhgR4Vez3twLwkFJmoUxC51Q'},
+    headers={'X-Api-Key': 'Your Api_key paste here'},
 )
 if response.status_code == requests.codes.ok:
     with open('no-bg.png', 'wb') as out:
